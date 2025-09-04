4373e93ff2fd285a16a0a0d9f019307d9b9b3d2d
Altaïr <notstaylost@gmail.com>
2025-09-04 15:28:46 +0200

    add react app url to api cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -12,7 +12,7 @@ const app = new Hono();
 app.use(
   '*',
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:8780', 'https://react-hono-proxy.pages.dev'],
     allowHeaders: ['Content-Type', 'Authorization'],
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
